445d2b4c3004139bc3230d1f787023a38a3614b1
abhinnvyas <abhinnvyas2@gmail.com>
2025-02-08 19:16:05 +0530

    api url changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || 'https://your-server-url.com',
+});
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("authData")) {
